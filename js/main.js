8cb91d72ed200f8013cd61b27428c05591fe2d36
tquiet <t.tz@icloud.com>
2020-08-19 21:10:08 +0300

    added adaptive version

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-$(function(){
-$('.photo__slider').slick({
-  prevArrow:'<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
-  nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
-});
+$(function () {
+  $('.photo__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+  });
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
 });
